7753713daa870082836772ec036e414fedf2406c
Abdelaziz Belhaj <abel-haj@s1r4p3.1337.ma>
2021-07-15 12:59:08 +0100

    mariadb

---
+++ srcs/requirements/mariadb/install_mariadb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export DEBIAN_FRONTEND="noninteractive"
+debconf-set-selections <<< "mariadb-server mysql-server/root_password password root"
+debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password root"
+apt-get -y install mariadb-server
