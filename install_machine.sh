b6febfacb0adf2b1cf4c9a551ddc30239415764b
Abdelaziz Belhaj <abel-haj@s2r4p2.1337.ma>
2021-09-25 13:20:46 +0100

    few updates

--- install_machine.sh
+++ install_machine.sh
@@ -4,26 +4,27 @@
 # to speed things up
 
 apt-get -y update;
-# apt-get -y upgrade;
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get -y upgrade;
 
-apt-get -y vim curl;
-# Chrome
-curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/chrome.deb
-dpkg -i /tmp/chrome.deb
-rm -rf /tmp/chrome.deb
+# Docker
+apt-get -y remove docker docker-engine docker.io
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
 
 # Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Docker
-apt-get remove docker docker-engine docker.io
-apt install docker.io
-systemctl start docker
-systemctl enable docker
 # Docker-compose
-apt-get install docker-compose
+apt-get install -y docker-compose
+
+apt-get install -y vim curl;
+
+# Chrome
+curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/chrome.deb
+dpkg -i /tmp/chrome.deb
+rm -rf /tmp/chrome.deb
 
 mkdir -p /home/abel-haj/data
 
