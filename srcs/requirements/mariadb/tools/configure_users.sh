63db1bb892866a3adb65ec76e67e1750d1365fdf
Abdelaziz Belhaj <abel-haj@s2r4p2.1337.ma>
2021-10-02 19:15:02 +0100

    added some configs

--- srcs/requirements/mariadb/tools/configure_users.sh
+++ srcs/requirements/mariadb/tools/configure_users.sh
@@ -2,11 +2,11 @@
 service mysql start;
 
 # New user for WordPress
-mysql -e "CREATE USER 'abel-haj'@localhost IDENTIFIED BY 'abel-haj';"
+mysql -e "CREATE USER 'abel-haj'@'%' IDENTIFIED BY 'abel-haj';"
 mysql -e "CREATE DATABASE wordpress;"
 
 # Saving user to avoid getting error when adding his privileges
 mysql -e "FLUSH PRIVILEGES;"
 
 # # Grant access only to WordPress database
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abel-haj'@localhost;"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abel-haj'@'%';"
