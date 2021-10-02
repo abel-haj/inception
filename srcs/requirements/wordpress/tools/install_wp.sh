a638752d9c220f6d13b8c477973c4720666ddebd
Abdelaziz Belhaj <abel-haj@s2r4p2.1337.ma>
2021-10-02 20:16:09 +0100

    push

--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -3,8 +3,9 @@ curl -q --progress-bar -o /tmp/latest.tar.gz https://wordpress.org/latest.tar.gz
 tar -zxf /tmp/latest.tar.gz -C /tmp
 mkdir -p /var/www/html
 mv /tmp/wordpress/ /var/www/html/
-chmod 666 -R /var/www/html/wordpress
-cp /wp-config.php /var/www/html/wordpress/
+chmod 777 -R /var/www/html/wordpress
+cp /tmp/wp-config.php /var/www/html/wordpress/
+rm /var/www/html/wordpress/wp-config-sample.php
 echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 service php7.3-fpm start
